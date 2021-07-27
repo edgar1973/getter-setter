b01480308540e60fdaa449ddb8f28f842aa6986a
Edgar <max.edgar98@gmail.com>
2021-07-27 15:35:34 +0300

    final commit

--- js/main.js
+++ js/main.js
@@ -9,35 +9,55 @@ const person = {
   get name() {
     return this.__name;
   },
-  set name(value){
+  set name(value) {
+    Object.defineProperty(this, '__name', { writable: true });
     this.__name = value;
+    Object.defineProperty(this, '__name', { writable: false });
   },
   get gender() {
     return this.__gender;
   },
   set gender(value) {
+    Object.defineProperty(this, '__gender', { writable: true });
     if (value === 'male' || value === 'female' || value === 'non-binary' || value === 'non-binary' || value === 'transgender' || value === 'Intersex' || value === 'n/a') {
       this.__gender = value;
     } else {
       console.log('ERROR');
     }
+    Object.defineProperty(this, '__gender', { writable: false });
   },
   get age() {
     return this.__age;
   },
   set age(value) {
+    Object.defineProperty(this, '__age', { writable: true });
     if (value > 0 && value < 120) {
       this.__age = value;
     } else {
       console.log('ERROR');
     }
+    Object.defineProperty(this, '__age', { writable: false });
   }
 }
 
-person.age = 1200;
-person.gender = 'dsfg';
-console.log(person);
-person.name = 'Vasiliy';
-person.age = 25;
-person.gender = 'male';
-console.log(person);
+Object.defineProperties(person, {
+  '__name': {
+    value: name,
+    writable: false,
+  },
+  '__age': {
+    value: age,
+    writable: false,
+  },
+  '__gender': {
+    value: gender,
+    writable: false,
+  },
+});
+
+person.__name = '11111';
+person.gender = 'female';
+
+console.log(person.name);
+console.log(person.__name);
+console.log(person.__gender);
